a0c13385ecc818c94682688c545266141177baf1
yangximei <yangximei@tjhq.com>
2019-02-27 16:05:53 +0800

    first Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <router-view/>
+        <ul class="footer">
+            <li class="icons"><router-link :to="{name:'list'}">新闻列表</router-link></li>
+            <li class="icons"><router-link :to="{name:'user'}">个人中心</router-link></li>
+        </ul>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "home"
+    }
+</script>
+
+<style lang="stylus" scoped>
+    li{
+        list-style none
+    }
+.footer{
+    position fixed
+    width 100%
+    height 60px
+    line-height 60px
+    left 0
+    bottom 0
+    display flex
+    flex-flow row nowrap
+    justify-content space-around
+}
+    .icons{
+        font-size 16px
+        flex 1
+        text-align center
+        border-top 1px solid green
+        a{
+            color green
+            display block
+            text-decoration none
+            &.active{
+                color #fff
+                background-color green
+            }
+        }
+    }
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button @click="toAdd()">添加</button><br/>
+        User
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "user",
+        methods:{
+            toAdd(){
+                this.$router.push('/add');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
